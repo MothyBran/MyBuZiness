47b30cc4075eec6e0f5ef16f261ac4861e66a21d
MothyBran <Timothybran@gmail.com>
2025-08-16 19:35:26 +0200

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -33,6 +33,7 @@ export default function Header() {
         <Link href="/" style={linkStyle("/")}>Start</Link>
         <Link href="/kunden" style={linkStyle("/kunden")}>Kunden</Link>
         <Link href="/produkte" style={linkStyle("/produkte")}>Produkte</Link>
+        <Link href="/belege" style={linkStyle("/belege")}>Belege</Link>
         <Link href="/rechnungen" style={linkStyle("/rechnungen")}>Rechnungen</Link>
         <Link href="/einstellungen" style={linkStyle("/einstellungen")}>Einstellungen</Link>
       </nav>
