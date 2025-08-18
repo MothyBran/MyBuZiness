05b2d5b3af504963c5f27e2f5a3ed8a9fb7279a5
MothyBran <Timothybran@gmail.com>
2025-08-18 18:50:43 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: false,
-  // Wichtig: nothing special needed for App Router on Next 13/14,
-  // aber falls du älter bist, hier kein appDir mehr nötig.
-};
-
-module.exports = nextConfig;
