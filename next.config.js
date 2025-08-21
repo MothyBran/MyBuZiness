a81ab71974fc5f963695c780d1d79f2828858d4d
MothyBran <Timothybran@gmail.com>
2025-08-21 19:41:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-  // WICHTIG: KEIN output: "standalone"
+  reactStrictMode: true,
+  experimental: { appDir: true },
 };
+
 module.exports = nextConfig;
