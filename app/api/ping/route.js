8fbe3651fae93969cd501dd5458f7218e5a236a6
MothyBran <Timothybran@gmail.com>
2025-08-21 19:43:42 +0200

    Delete app directory

--- app/api/ping/route.js
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return new Response("pong", { headers: { "content-type": "text/plain" } });
-}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-"use client";
-
-import "../src/theme/theme.css";        // <- unser globales Theme
-import App from "../src/App";           // <- die neue App aus /src
-
-export default function Page() {
-  return <App />;
-}
