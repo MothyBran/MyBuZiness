c1ecfdb42bf70a3426c3389d9879905432aa2cf2
MothyBran <Timothybran@gmail.com>
2025-08-22 11:51:32 +0200

    Update page.tsx

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,8 +1,4 @@
 // app/[...slug]/page.tsx
 "use client";
-
 import App from "../../src/App";
-
-export default function CatchAll() {
-  return <App />;
-}
+export default function CatchAll(){ return <App/>; }
