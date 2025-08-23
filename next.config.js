418a973f2b932da10841851503ba81fb29fb804a
MothyBran <Timothybran@gmail.com>
2025-08-23 11:11:36 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  // nichts Ungewöhnliches wie basePath/assetPrefix/experimental.appDir
-};
-module.exports = nextConfig;
