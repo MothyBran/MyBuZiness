43a9d3490235d138091327c22757fd438f992dfe
MothyBran <Timothybran@gmail.com>
2025-08-23 20:38:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
-  trailingSlash: false,
-
+  // wenn du Images-Loader, Domains etc. nutzt, hier ergänzen
 };
 
 module.exports = nextConfig;
