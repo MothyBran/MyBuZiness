95b6d37a9599417615f0fe4beec8ed4eb7a59a09
MothyBran <Timothybran@gmail.com>
2025-08-23 22:40:30 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
-  // wenn du Images-Loader, Domains etc. nutzt, hier ergänzen
-};
-
-module.exports = nextConfig;
