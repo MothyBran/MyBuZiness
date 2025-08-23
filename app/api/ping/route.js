e755830c05cad0e9e02dda45808c7e845c015aee
MothyBran <Timothybran@gmail.com>
2025-08-23 22:39:16 +0200

    Delete app directory

--- app/api/ping/route.js
+++
@@ -1,7 +0,0 @@
-// app/api/ping/route.js
-export async function GET() {
-  return new Response(JSON.stringify({ ok: true, pong: true }, null, 2), {
-    status: 200,
-    headers: { "content-type": "application/json" },
-  });
-}
